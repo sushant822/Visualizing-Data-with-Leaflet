edde710845cf4aea9d41ddb98f77417fd7a1e1f1
Sushant Deshpande <41443708+sushant822@users.noreply.github.com>
2020-09-20 13:00:35 -0600

    testing

--- static/js/logic.js
+++ static/js/logic.js
@@ -31,23 +31,46 @@ function magColor(mag) {
                    '#1A9850';
 }
 
-//var myMap = L.map("map").setView([29.2996437,1.832259],2);
-var myMap = L.map("map", {
-  center: [
-      29.2996437,1.832259
-  ],
-  zoom: 2,
-  //layers: [streetmap, earthquakes]
-});
-
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/light-v10",
     accessToken: API_KEY
-  }).addTo(myMap);
+  })
+
+  var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+// Only one base layer can be shown at a time
+var baseMaps = {
+  Light: lightMap,
+  Dark: darkMap
+};
+
+// Overlays that may be toggled on or off
+var overlayMaps = {
+  //Cities: cityLayer
+};
+
+//var myMap = L.map("map").setView([29.2996437,1.832259],2);
+var myMap = L.map("map", {
+  center: [
+      29.2996437,1.832259
+  ],
+  zoom: 2,
+  layers: [lightMap, darkMap]
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
 
   /*  TESTING  */
 /*
