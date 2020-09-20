ce47d0f04fbe223f41ab484548bf4c1e2f299fac
Sushant Deshpande <41443708+sushant822@users.noreply.github.com>
2020-09-20 12:10:55 -0600

    testing

--- static/js/logic.js
+++ static/js/logic.js
@@ -42,6 +42,17 @@ var myMap = L.map("map", {
 
 /*  TESTING  */
 
+function getColor(d) {
+  return d > 1000 ? '#800026' :
+         d > 500  ? '#BD0026' :
+         d > 200  ? '#E31A1C' :
+         d > 100  ? '#FC4E2A' :
+         d > 50   ? '#FD8D3C' :
+         d > 20   ? '#FEB24C' :
+         d > 10   ? '#FED976' :
+                    '#FFEDA0';
+}
+
 var legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function (map) {
